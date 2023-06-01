56ed24c4bc5ab8922d7b56b6e8fdf40f3c80f95c
AugustoAlmondes <augusto7666@gmail.com>
2023-06-01 10:33:31 -0300

    Testando tempo

--- main.c
+++ main.c
@@ -1,89 +1,99 @@
-#include<stdio.h>
-#include<stdio.h>
-#include<string.h>
-#include"curso.h"
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include "curso.h"
+#include <time.h>
 
-int main() {
+int main()
+{
 
     int codigo;
 
     Curso *arvoreCursos = CriarArvore_Curso();
     Disciplina *arvoreDisciplinas = CriarArvore_Dsc();
-    
+
+    time_t ini, fim;
+    double tempo;
+
+    ini = time(NULL);
     arvoreCursos = adcCurso();
-    
-    //primeira questao - C - 1
+    fim = time(NULL);
+
+    tempo = difftime(fim, ini);
+
+    printf("Tempo: %.20lf\n", tempo);
+
+    // primeira questao - C - 1
     printf("----- Todos os Cursos -----\n");
     Exibir_Todos_Cursos(arvoreCursos);
 
-    //primeira questao - C - 2
+    // primeira questao - C - 2
     printf("----- Dados de um Curso -----\n");
     printf("Digite o codigo do curso que deseja ver os dados: ");
-    scanf(" %d",&codigo);
-    ExibirDados_Curso(arvoreCursos,codigo);
+    scanf(" %d", &codigo);
+    ExibirDados_Curso(arvoreCursos, codigo);
 
-    //primeira questao - c - 3
+    // primeira questao - c - 3
     printf("----- Exibir Cursos por Blocos -----\n");
     int quantidade;
     printf("Digite a quantidade de blocos que deseja procurar: ");
     scanf(" %d", &quantidade);
-    ExibirCursoBloco(arvoreCursos,quantidade);
+    ExibirCursoBloco(arvoreCursos, quantidade);
 
     arvoreCursos = adcDsc();
-    
-    //primeira questao - C - 4
+
+    // primeira questao - C - 4
     printf("----- Disciplinas do Curso -----\n");
     printf("Digite o codigo do curso para ver as disciplinas: ");
-    scanf(" %d",&codigo);
-    ExibirDsc_por_Curso(arvoreCursos,codigo);
+    scanf(" %d", &codigo);
+    ExibirDsc_por_Curso(arvoreCursos, codigo);
 
-    //primeira questao - C - 5
+    // primeira questao - C - 5
     printf("---- Dados de uma unica Disciplina -----\n");
     int codigo_dsc;
     printf("Digite o codigo do Curso: ");
-    scanf(" %d",&codigo);
+    scanf(" %d", &codigo);
     printf("Digite o codigo da Disciplina: ");
-    scanf(" %d",&codigo_dsc);
-    Imprimir_dsc(arvoreCursos,codigo,codigo_dsc);
+    scanf(" %d", &codigo_dsc);
+    Imprimir_dsc(arvoreCursos, codigo, codigo_dsc);
 
-    //primeira questao - C - 6
+    // primeira questao - C - 6
     printf("----- Dados das Disciplinas de acordo com um bloco -----\n ");
     int bloco;
     printf("Digite codigo do bloco: ");
-    scanf(" %d",&bloco);
+    scanf(" %d", &bloco);
     printf("Digite codigo do curso: ");
-    scanf(" %d",&codigo);
+    scanf(" %d", &codigo);
 
-    BuscarCurso_para_ExibirDsc_por_Bloco(arvoreCursos,codigo,bloco);
+    BuscarCurso_para_ExibirDsc_por_Bloco(arvoreCursos, codigo, bloco);
 
-    //primeira questao - C - 7
+    // primeira questao - C - 7
     printf("----- Dados das Disciplinas por Carga horaria -----\n");
     int cargahr;
     printf("Digite a Carga Horaia: ");
-    scanf(" %d",&cargahr);
+    scanf(" %d", &cargahr);
     printf("Digite o Codigo do Curso: ");
-    scanf(" %d",&codigo);
-    BuscarCurso_para_ExibirDsc_por_CH(arvoreCursos,codigo,cargahr);
-     
-    //primeira questao - C - 8
+    scanf(" %d", &codigo);
+    BuscarCurso_para_ExibirDsc_por_CH(arvoreCursos, codigo, cargahr);
+
+    // primeira questao - C - 8
     printf("----- Remover Disciplina -----\n");
     printf("Digite o codigo da Disciplina: ");
-    scanf(" %d",&codigo_dsc);
+    scanf(" %d", &codigo_dsc);
     printf("Digite o codigo do Curso: ");
-    scanf(" %d",&codigo);
-    BuscarCurso_para_RemoverDsc(&arvoreCursos,codigo,codigo_dsc);
+    scanf(" %d", &codigo);
+    BuscarCurso_para_RemoverDsc(&arvoreCursos, codigo, codigo_dsc);
     printf("----- Disciplinas do Curso -----\n");
     printf("Digite o codigo do curso para ver as disciplinas: ");
-    ExibirDsc_por_Curso(arvoreCursos,codigo);
+    ExibirDsc_por_Curso(arvoreCursos, codigo);
 
-    //primeira questao - C - 9
+    // primeira questao - C - 9
     printf("---- Remover Curso ----\n");
     printf("Digite o codigo do curso: ");
     scanf(" %d", &codigo);
-    RemoverCurso(&arvoreCursos,codigo);
+    RemoverCurso(&arvoreCursos, codigo);
     printf("----- Todos os Cursos -----\n");
     Exibir_Todos_Cursos(arvoreCursos);
 
-     
-return 0;
+    return 0;
 }
